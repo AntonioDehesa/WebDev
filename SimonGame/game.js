41836618fd12ad5317e44af447a532b31604e4f7
AntonioDehesa <hdehesaortiz@gmail.com>
2021-06-30 10:54:10 -0500

    Completed the challenge, and started the next section: Unix Command Line

--- SimonGame/game.js
+++ SimonGame/game.js
@@ -1,6 +1,8 @@
 var buttonColours = ["red","blue","green","yellow"];
 var gamePattern = [];
+var level = 0;
 var i = 0;
+var started = false;
 
 function playSound(sound)
 {
@@ -13,47 +15,78 @@ function nextSequence()
     var random = Math.floor(Math.random() * 4);
     gamePattern.push(buttonColours[random]);
     gamePattern.forEach(color => {
-        setTimeout(function()
-        {
-            $(this).addClass("pressed");
-        },100);
-        $(this).removeClass("pressed");
-        playSound(color);
+        setTimeout(function () {
+            playSound(color);
+            $("#" + buttonColours[random]).fadeIn(100).fadeOut(100).fadeIn(100);
+        }, 300);
     });
+    level++;
+    $("#level-title").text("Level " + level);
+}
+
+
+function error()
+{
+    started = false;
+    $('body').addClass("game-over");
+    playSound("wrong");
+    $("#level-title").text("Game Over, Press A Key to Start");
+    setTimeout(function () 
+    {
+        $("body").removeClass("game-over");
+    }, 200);
+    gamePattern = [];
 }
 
 $(document).ready(function()
 {
-    nextSequence();
+    $(document).keypress(function()
+    {
+        started = true;
+        level = 0;
+        nextSequence();
+        $("#level-title").text("Level " + level);
+        $('body').removeClass("game-over");
+    }
+    );
     $(".btn").on("click", function()
     {
-        var id = $(this).attr("id");
-        console.log(gamePattern[i]);
-        console.log(id);
-        if(i < gamePattern.length)
+        if (started)
         {
-            if(gamePattern[i] !== id)
+            var id = $(this).attr("id");
+            console.log(gamePattern[i]);
+            console.log(id);
+            if(i < gamePattern.length)
             {
-                $(document).addClass("game-over");
-                playSound("wrong");
+                if(gamePattern[i] !== id)
+                {
+                    error();
+                }
+                else
+                {
+                    $("#" + id).addClass("pressed");
+                    setTimeout(function()
+                    {
+                        $("#" + id).removeClass("pressed");
+                    },100);
+                    //$("#" + id).fadeIn(100).fadeOut(100).fadeIn(100);
+                    playSound(id);
+                }
+                i++;
+                if ( i == gamePattern.length)
+                {
+                    i = 0;
+                    nextSequence();
+                }
             }
             else
             {
                 setTimeout(function()
                 {
-                    $(this).addClass("pressed");
+                    nextSequence();
                 },100);
-                $(this).removeClass("pressed");
-                //$("#" + id).fadeIn(100).fadeOut(100).fadeIn(100);
-                playSound(id);
+                i = 0;
             }
-            i++;
-            nextSequence();
-        }
-        else
-        {
-            i = 0;
-        }
+            }
     });
 });
-
