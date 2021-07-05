3877ef0631a445e87a0fc0e9859ff2bbd8f5e6f6
AntonioDehesa <hdehesaortiz@gmail.com>
2021-07-05 10:55:46 -0500

    Started the nodejs module

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,4 +1,14 @@
 //jshint esversion:6
-const fs = require('fs');
+/*const fs = require('fs');
 
-fs.copyFileSync("file1.txt", "file2.txt");
+fs.copyFileSync("file1.txt", "file2.txt");*/
+
+var superheroes = require("superheroes");
+
+var mySuperheroeName = superheroes.random();
+
+var villains = require("supervillains");
+
+var villain = villains.random();
+
+console.log(mySuperheroeName + " vs " + villain);
