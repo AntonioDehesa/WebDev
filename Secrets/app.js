6042b0c0b2f817d920c5f629953a8f4dd3f3f9e6
AntonioDehesa <hdehesaortiz@gmail.com>
2022-01-22 15:34:57 -0600

    Finally managed to make passport work! It was way harder than expected. I had lots of issues: bad requests, the authentication froze, etc. In the end, I found out that passport needs bodyParser, even if we do not use it for anything else

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,99 +1,94 @@
-require('dotenv').config(); //Required to store API keys in env and not in code
+require('dotenv').config();
 const express = require("express");
-//const { response } = require("express");
-const app = express();
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const mongoose = require("mongoose");
-//const encrypt = require("mongoose-encryption");
-//const sha256 = require("js-sha256");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
-//const ejs = require("ejs");
-
-app.set('view engine', 'ejs');
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+var LocalStrategy = require('passport-local').Strategy;
+const app = express();
 
 app.use(express.static("public"));
-
-mongoose.connect("mongodb://localhost:27017/userBD",{useNewUrlParser: true});
-
-const userSchema = new mongoose.Schema({
-    email: {
-        type: String, 
-        required: true},
-    password: {type: String,
-    required: true}
-});
-
+app.set('view engine', 'ejs');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+//passport needs bodyParser, even if express does not need it anymore. 
 /*Encryption*/
 const secret = process.env.secretString;
 
-//userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+app.use(session({
+  secret: secret,
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String, 
+        required: true},
+    /*password: {type: String,
+    required: true}//we do not need to specify the password here, as passport does it for us
+    password: String*/
+});
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
 ///methods
 app.get("/", function (req,res) {
     res.render("home");
 });
 
-app.get("/login", function (req,res) {
-    res.render("login");
+app.route("/login").get(function (req,res) {
+  res.render("login");
+}).post(function (req,res) {
+  passport.authenticate("local")(req,res,function(){
+    res.redirect("/secrets");
+  });
 });
 
-app.get("/register", function (req,res) {
-    res.render("register");
+app.get("/secrets", function (req,res) {
+  if(req.isAuthenticated())
+  {
+    res.render("secrets");
+  }
+  else
+  {
+    res.redirect("/login");
+  }
 });
 
-app.post("/register", function (req,res) {
-    bcrypt.genSalt(saltRounds, function(err, salt) {
-        bcrypt.hash(req.body.password, salt, function(err, hash) {
-            const newUser = new User({
-                email: req.body.username,
-                password: hash
-            });
-            newUser.save(function (err) {
-                if(err)
-                {
-                    console.log("There was an error when creating a new user");
-                    console.log(err);
-                }
-                else
-                {
-                    res.render("secrets");
-                }
-            });
-        });
-    });
-    
+app.route("/register").get(function (req,res) {
+  res.render("register");
+}).post(function(req, res){
+  User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+    if (err) {
+      console.log(err);
+      res.render("register");
+    } 
+    passport.authenticate("local")(req,res,function(){
+      res.redirect("/secrets");
+  })    
+  });
+  
 });
-
-app.post("/login", function (req,res) {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({email: username}, function (err,foundUser) {
-        if(err)
-        {
-            console.log("There was an error when geeting a registered user");
-            console.log(err);
-        }
-        else
-        {
-            if(foundUser)
-            {
-                bcrypt.compare(password, foundUser.password, function (err,result) {
-                    if(result)
-                    {
-                        res.render("secrets");
-                    }
-                });
-            }
-        }
-    })
-})
 
 app.listen(3000, function () {
-    console.log("Server started on port 3000");
+  console.log("Server started on port 3000");
 });
