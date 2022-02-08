5f63451512120b7dfb2a043292881a4ad71a4049
AntonioDehesa <hdehesaortiz@gmail.com>
2022-02-08 00:19:57 -0600

    Continued the webdev course

--- React/keeper-app-part-1-starting/src/components/App.jsx
+++ React/keeper-app-part-1-starting/src/components/App.jsx
@@ -2,17 +2,20 @@ import React from "react"
 import Header from "./subComponents/Header.jsx"
 import Footer from "./subComponents/Footer.jsx"
 import Note from "./subComponents/Note.jsx"
+
+import notes from "../notes.js"
+
 function App() {
-    return <div>
+    return (<div>
         <div>
             <Header/>
         </div>
         <h1>Hello App</h1>
-        <Note/>
+        {notes.map(note => <Note key = {note.key} Title={note.title} Content={note.content}/>)}
         <div>
             <Footer/>
         </div>
-    </div>
+    </div>);
 }
 
 export default App
---
+++ React/keeper-app-part-1-starting/src/notes.js
@@ -0,0 +1,20 @@
+const notes = [{
+    key: 1,
+    title: "Delegation",
+    content: "Q. How many programmers does it take to change a light bult? A. None - Its a hardware problem"
+},
+{
+    key: 2,
+    title: "Loops",
+    content: "How to keep..."
+},{
+    key: 3,
+    title: "Arrays",
+    content: "Why did..."
+},{
+    key: 4,
+    title: "Hardware vs Software",
+    content: "Whats the difference..."
+}];
+
+export default notes
